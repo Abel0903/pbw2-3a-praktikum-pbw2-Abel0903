baec0397d329f19ec640cd9c6e834239e1cede7a
Irfan Azmii Musyaffa <irfanazmiim@gmail.com>
2026-09-19 18:45:13 +0700

    menambah halaman daftar menu

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BerandaController::class, 'index'])->name('beranda');
+Route::get('/menu', [MenuController::class, 'index'])->name('menu');
